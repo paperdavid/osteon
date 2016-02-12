5a8ba6b7516f08b51b4e7bfafc796849e45b9b59
David <paperdavid@gmail.com>
2016-02-12 18:28:10 +0300

    carousel + base template

--- src/js/custom.js
+++ src/js/custom.js
@@ -7,5 +7,24 @@ $(document).ready(function() {
     /* Custom */
 
     //= ./common/likely.js
+    //= ./common/owl.carousel.min.js
+
+$('.loop').owlCarousel({
+    center: true,
+    items:3,
+    loop:true,
+    margin:10,
+    responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+    }
+});
 
 });
