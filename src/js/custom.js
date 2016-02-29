94ba3664fb64b2c0032924de1c2d53d7804779a5
David <paperdavid@gmail.com>
2016-02-29 16:54:30 +0300

    fixes+programs

--- src/js/custom.js
+++ src/js/custom.js
@@ -9,22 +9,21 @@ $(document).ready(function() {
     //= ./common/likely.js
     //= ./common/owl.carousel.min.js
 
-$('.loop').owlCarousel({
-    center: true,
-    items:3,
-    loop:true,
-    margin:10,
-    responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:3
-          }
-    }
-});
-
+  $('.loop').owlCarousel({
+      center: true,
+      items:3,
+      loop:true,
+      margin:10,
+      responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+      }
+  });
 });
